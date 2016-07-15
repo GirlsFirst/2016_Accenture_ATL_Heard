1149f44aba0a9e64d3366df94e29b0b55ac0c876
MadisonRussell <madisonrussell@gmail.com>
2016-07-15 14:38:07 -0400

    more meteor changes, plus templates added

---
+++ README.md
@@ -0,0 +1,9 @@
+# 2016_Accenture_ATL_Heard
+connecting LGBTQ+ youth and/or youths with mental illness to anonymous penpals
+
+
+Debbie
+elissa
+
+License:
+MIT License
---
+++ heard/client/signup.js
@@ -0,0 +1,10 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+
+
+Router.route("/signup",{
+	template: "signup"
+});
