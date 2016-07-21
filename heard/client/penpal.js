44bdebe5f9dfb844ca954aaac782804a4ad6e1fa
Debbie1352 <laoguai2000@gmail.com>
2016-07-21 11:03:22 -0400

    penpal redirect

--- heard/client/penpal.js
+++ heard/client/penpal.js
@@ -3,5 +3,7 @@ Template.findPenpal.events({
 		var penpal =$(e.currentTarget).attr("name");
 		console.log(penpal);
 		Meteor.users.update({_id:Meteor.user()._id},{$set:{'profile.penpal':penpal}});
+
+		FlowRouter.go('/');
 	} 
 })
